58088453c662bb8d68768e4c60992f123ecae963
Damian Augusto <60717309+damianaugusto1501@users.noreply.github.com>
2022-08-17 11:23:25 -0300

    Update profundizacion_3.py

--- ejercicios_profunidzacion/profundizacion_3.py
+++ ejercicios_profunidzacion/profundizacion_3.py
@@ -10,6 +10,9 @@
 # de mucho más tiempo para abordar estos temas por su cuenta.
 # Requiere mayor tiempo de dedicación e investigación autodidacta.
 
+import string
+
+
 notas = [70, 82, -1, 65, 55, 67, 87, 92, -1]
 
 # IMPORTANTE: NO borrar los comentarios en VERDE o NARANJA
@@ -41,18 +44,48 @@ print("Mi organizador académico (#_#)")
 
 # Para calcular el promedio primero debe obtener la suma
 # de todas las notas, que irá almacenando en esta variable
-sumatoria = 0           # Ya le hemos inicializado en 0
 
+sumatoria = 0           # Ya le hemos inicializado en 0
+promedio = 0
 cantidad_notas = 0      # Aquí debe contar cuantas notas válidas encontró
 cantidad_ausentes = 0   # Aquí debe contar cuantos ausentes hubo
+notafinal = string
 
 # Realice aquí el bucle para recorrer todas las notas
 # y cacular la sumatoria
+
+for puntaje in notas :
+
+    if puntaje > 0 :
+        sumatoria += puntaje
+        cantidad_notas += 1
+
+    elif puntaje < 0 :
+        cantidad_ausentes += 1
+
 
 # Terminado el bucle calcule el promedio como
 # promedio = sumatoria / cantidad_notas
 
 # Utilice la nota promedio calculada y transformela
 # a calificación con letras, imprima en pantalla el resultado
+
+promedio = (sumatoria / cantidad_notas)
+
+if promedio > 90 :
+    notafinal = "A"
+elif promedio > 80 :
+    notafinal = "B"
+elif promedio > 70 :
+    notafinal = "C"
+elif promedio > 60 :
+    notafinal = "D"
+else :
+    notafinal = "F"
+
+print("La nota final del alumno fue una :",notafinal)
+print("Se contabilizo",cantidad_ausentes , "Ausentes")
+
+
 
 # Imprima en pantalla al cantidad de ausentes
